eef37ed61c8c6631cfa2ec0a10fa02185611d58e
BurhanuddinAlfauzi <burhanuddinalfauzi@gmail.com>
2022-02-07 10:29:38 +0700

    egipebrian

---
+++ src/components/button.js
@@ -0,0 +1,9 @@
+function Button() {
+    return (
+        <div className="text-center">
+            <a className="font-semibold text-yellow-300 border border-yellow-300 rounded-full px-6 py-2">Kontak</a>
+          </div>
+    );
+}
+
+export default Button;
---
+++ src/components/logo.js
@@ -0,0 +1,8 @@
+function Logo() {
+    return(
+        <div className="logo text-pink text-2xl text-center uppercase font-semibold">Egi</div>
+        
+    );
+}
+
+export default Logo;
